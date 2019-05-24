d889d856c12e00f259e03ea31a13ed012fc67cde
Maximilian Kroker Deister <maxikd@hotmail.com>
2019-05-24 00:57:21 -0300

    Added tests

---
+++ Payment API/PaymentAPITests/Helpers/AmountsTests.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payment_API.Enums;
+using Payment_API.Helpers;
+using Payment_API.Model;
+using System;
+
+namespace PaymentAPITests.Helpers
+{
+    [TestClass]
+    public class AmountsTests
+    {
+        private const string ACQUIRER = "A";
+        private const string INVALID_CARD_BRAND = "APPLE";
+        private const string INVALID_TRANSACTION_TYPE = "PHONE";
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            MDRData.GetMDR();
+        }
+
+        #region Exception tests
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Compute_Amount_0_ThrowsArgumentOutOfRangeException()
+        {
+            Amounts.ComputeNetAmount(0, ACQUIRER, CardBrand.Visa, TransactionType.Credit);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Compute_Acquirer_Null_ThrowsArgumentNullException()
+        {
+            Amounts.ComputeNetAmount(100, null, CardBrand.Visa, TransactionType.Credit);
+        }
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Compute_Acquirer_Empty_ThrowsArgumentNullException()
+        {
+            Amounts.ComputeNetAmount(100, string.Empty, CardBrand.Visa, TransactionType.Credit);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Compute_CardBrand_Null_ThrowsArgumentNullException()
+        {
+            Amounts.ComputeNetAmount(100, ACQUIRER, null, TransactionType.Credit);
+        }
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Compute_CardBrand_Invalid_ThrowsArgumentOutOfRangeException()
+        {
+            Amounts.ComputeNetAmount(100, ACQUIRER, INVALID_CARD_BRAND, TransactionType.Credit);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Compute_TransactionType_Null_ThrowsArgumentNullException()
+        {
+            Amounts.ComputeNetAmount(100, ACQUIRER, CardBrand.Visa, null);
+        }
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [TestMethod]
+        public void Compute_TransactionType_Invalid_ThrowsIndexOutOfRangeException()
+        {
+            Amounts.ComputeNetAmount(100, ACQUIRER, CardBrand.Visa, INVALID_TRANSACTION_TYPE);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Compute_Transaction_Null_ThrowsArgumentNullException()
+        {
+            Amounts.ComputeNetAmount(null);
+        }
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Compute_TransactionAmount_0_ThrowsArgumentOutOfRangeException()
+        {
+            var transaction = new Transaction(0, ACQUIRER, CardBrand.Mastercard, TransactionType.Credit);
+
+            Amounts.ComputeNetAmount(transaction);
+        }
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Compute_TransactionAcquirer_Null_ThrowsArgumentNullException()
+        {
+            var transaction = new Transaction(100, null, CardBrand.Mastercard, TransactionType.Credit);
+
+            Amounts.ComputeNetAmount(transaction);
+        }
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Compute_TransactionAcquirer_Empty_ThrowsArgumentNullException()
+        {
+            var transaction = new Transaction(100, string.Empty, CardBrand.Mastercard, TransactionType.Credit);
+
+            Amounts.ComputeNetAmount(transaction);
+        }
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Compute_TransactionCardBrand_Null_ThrowsArgumentNullException()
+        {
+            var transaction = new Transaction(100, ACQUIRER, null, TransactionType.Credit);
+
+            Amounts.ComputeNetAmount(transaction);
+        }
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Compute_TransactionCardBrand_Invalid_ThrowsArgumentOutOfRangeException()
+        {
+            var transaction = new Transaction(100, ACQUIRER, INVALID_CARD_BRAND, TransactionType.Credit);
+
+            Amounts.ComputeNetAmount(transaction);
+        }
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Compute_TransactionTransactionType_Null_ThrowsArgumentNullException()
+        {
+            var transaction = new Transaction(100, ACQUIRER, CardBrand.Mastercard, null);
+
+            Amounts.ComputeNetAmount(transaction);
+        }
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [TestMethod]
+        public void Compute_TransactionTransactionType_Invalid_ThrowsIndexOutOfRangeException()
+        {
+            var transaction = new Transaction(100, ACQUIRER, CardBrand.Mastercard, INVALID_TRANSACTION_TYPE);
+
+            Amounts.ComputeNetAmount(transaction);
+        }
+        #endregion
+
+        #region Compute tests
+        [TestMethod]
+        public void Compute()
+        {
+            double amount = 1000,
+            netAssert = 973.5,
+            net;
+
+            net = Amounts.ComputeNetAmount(amount, "B", CardBrand.Mastercard, TransactionType.Credit);
+
+            Assert.AreEqual(netAssert, net);
+        }
+
+        [TestMethod]
+        public void Compute_Transaction()
+        {
+            var transaction = new Transaction(1000, "C", CardBrand.Visa, TransactionType.Debit);
+            double netAssert = 978.4,
+            net;
+
+            net = Amounts.ComputeNetAmount(transaction);
+
+            Assert.AreEqual(netAssert, net);
+        }
+        #endregion
+    }
+}
---
+++ Payment API/PaymentAPITests/Model/MDRDataTests.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payment_API.Enums;
+using Payment_API.Model;
+using System;
+
+namespace PaymentAPITests.Model
+{
+    [TestClass]
+    public class MDRDataTests
+    {
+        private const string ACQUIRER = "A";
+        private const string INVALID_ACQUIRER = "ABC";
+        private const string INVALID_CARD_BRAND = "APPLE";
+        private const string INVALID_TRANSACTION_TYPE = "PHONE";
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            MDRData.GetMDR();
+        }
+
+        #region Exception tests
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void GetFee_Acquirer_Null_ThrowsArgumentNullException()
+        {
+            MDRData.GetFee(null, CardBrand.Visa, TransactionType.Debit);
+        }
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void GetFee_Acquirer_Empty_ThrowsArgumentNullException()
+        {
+            MDRData.GetFee(string.Empty, CardBrand.Visa, TransactionType.Debit);
+        }
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void GetFee_Acquirer_Empty_ThrowsArgumentOutOfRangeException()
+        {
+            MDRData.GetFee(INVALID_ACQUIRER, CardBrand.Visa, TransactionType.Debit);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void GetFee_CardBrand_Null_ThrowsArgumentNullException()
+        {
+            MDRData.GetFee(ACQUIRER, null, TransactionType.Debit);
+        }
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void GetFee_CardBrand_Invalid_ThrowsArgumentNullException()
+        {
+            MDRData.GetFee(ACQUIRER, INVALID_CARD_BRAND, TransactionType.Debit);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void GetFee_TransactionType_Null_ThrowsArgumentNullException()
+        {
+            MDRData.GetFee(ACQUIRER, CardBrand.Visa, null);
+        }
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [TestMethod]
+        public void GetFee_TransactionType_Invalid_ThrowsIndexOutOfRangeException()
+        {
+            MDRData.GetFee(ACQUIRER, CardBrand.Visa, INVALID_TRANSACTION_TYPE);
+        }
+        #endregion
+
+        #region Fee tests
+        [TestMethod]
+        public void GetFee_Visa_Credit()
+        {
+            double feeAssert = 2.25,
+                fee;
+
+            fee = MDRData.GetFee("A", CardBrand.Visa, TransactionType.Credit);
+
+            Assert.AreEqual(feeAssert, fee);
+        }
+        [TestMethod]
+        public void GetFee_Visa_Debit()
+        {
+            double feeAssert = 2.08,
+                fee;
+
+            fee = MDRData.GetFee("B", CardBrand.Visa, TransactionType.Debit);
+
+            Assert.AreEqual(feeAssert, fee);
+        }
+        [TestMethod]
+        public void GetFee_Mastercard_Credit()
+        {
+            double feeAssert = 3.10,
+                fee;
+
+            fee = MDRData.GetFee("C", CardBrand.Mastercard, TransactionType.Credit);
+
+            Assert.AreEqual(feeAssert, fee);
+        }
+        [TestMethod]
+        public void GetFee_Mastercard_Debit()
+        {
+            double feeAssert = 1.75,
+                fee;
+
+            fee = MDRData.GetFee("B", CardBrand.Mastercard, TransactionType.Debit);
+
+            Assert.AreEqual(feeAssert, fee);
+        }
+        #endregion
+    }
+}
