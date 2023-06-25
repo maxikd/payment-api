48369cde5bc7836ccf98f60f81cc792d7e96b352
Maximilian Deister <maxikd@hotmail.com>
2023-06-25 10:52:49 +0100

    Adding missing service

--- Payments.API/Program.cs
+++ Payments.API/Program.cs
@@ -19,6 +19,8 @@ namespace Payments.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddControllers();
+
             builder.Services.AddSingleton<IMdrRepository, MdrRepository>();
 
             builder.Services.AddScoped<IMdrService, MdrService>();
